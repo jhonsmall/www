96554c5500f003bfc5eaf3d39c0a50f5a86700b6
Juan Chico <jchico@mushucruna.com>
2022-02-21 12:32:34 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import * as wasm from "hello-wasm";
+
+wasm.greet();
+console.log(wasm.suma(1, 2));
+console.log(wasm.suma(6, 8));
+console.log(wasm.suma(9, 9));
+console.log(wasm.suma(10, 10));
+console.log(wasm.suma(11, 11));
